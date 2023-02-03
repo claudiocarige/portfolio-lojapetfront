cea7ec5d4da057126af390ed16bfd09c60649e31
ClaudioCarige <ccarige@gmail.com>
2023-02-03 14:08:50 -0300

    Criado rota filha e ajustes de css e Html para Nav e first Mobile para projeto

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   showFiller = false;
-  constructor() { }
+  constructor( 
+    private route: Router
+  ) { }
 
   ngOnInit(): void {
+    this.route.navigate(['home']);
   }
 
 }

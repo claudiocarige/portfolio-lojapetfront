8fe934639eb59e3f3d0334f0d7b74636744407ff
ClaudioCarige <ccarige@gmail.com>
2023-02-04 12:59:13 -0300

    Rota Ligin criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './components/employes/employee-list/employee-list.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
-  {path:"", component: NavComponent, children : [
+  { path: "login", component:LoginComponent},
+  { path:"", component: NavComponent, children : [
         { path: "home",       component: HomeComponent},
         { path: "employees",  component: EmployeeListComponent}
   ] }

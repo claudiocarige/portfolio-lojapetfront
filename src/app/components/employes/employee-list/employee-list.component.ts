c3e6aa8daf4bdf6f7d750b9e31aaa87b3566c6b8
ClaudioCarige <ccarige@gmail.com>
2023-02-04 12:51:47 -0300

    Component Employee List criado, edição Css no Nav

--- src/app/components/employes/employee-list/employee-list.component.ts
+++ src/app/components/employes/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource           } from '@angular/material/table';
+import { Employee }  from 'src/app/models/dataEmployee'
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+  ELEMENT_DATA: Employee[] = [
+   { 
+    id: 1,
+    nome: 'Claudio',
+    cpf:'89496531504',
+    email: 'ccarige@mail.com',
+    password: '123456',
+    profile: ['0'],
+    criationDate: '04/02/2023'
+}
+]
+
+  displayedColumns: string[] = ['id', 'nome', 'email', 'cpf', 'acoes'];
+  dataSource = new MatTableDataSource<Employee>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
---
+++ src/app/models/dataEmployee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    id?:            any;
+    nome:        string;
+    cpf:         string;
+    email:       string;
+    password:       string;
+    profile:   string[];
+    criationDate:   any;
+
+}

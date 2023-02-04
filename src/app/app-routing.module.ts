f2aef571e00715d9fcce602d233cf974cca6e04e
ClaudioCarige <ccarige@gmail.com>
2023-02-04 08:57:26 -0300

    Rota para Employee List criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employes/employee-list/employee-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {path:"", component: NavComponent, children : [
-        { path: "home", component: HomeComponent}
+        { path: "home",       component: HomeComponent},
+        { path: "employees",  component: EmployeeListComponent}
   ] }
 ];
 

530f080d952e114248d56eb3ebe1ba9dc80b1e11
ClaudioCarige <ccarige@gmail.com>
2023-02-04 15:12:30 -0300

    Validação dos campos e butoon de Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credentials } from 'src/app/models/credentials';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+   
+  cred: Credentials = {
+    email: '',
+    password: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  password = new FormControl(null, Validators.minLength(6))
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampo():boolean{
+    if(this.email.valid && this.password.valid){
+      return true;
+    }
+    return false
   }
 
 }

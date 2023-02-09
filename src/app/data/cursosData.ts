e274a1b73061b1b69065efbd8632e84bdba69e24
ClaudioCarige <ccarige@gmail.com>
2023-02-09 00:40:32 -0300

    Criação dos arquivo de dados do protfólio

---
+++ src/app/data/cursosData.ts
@@ -0,0 +1,91 @@
+export const CursosData = [
+    {
+        "title" : "Angular e TypeScript Avançado",
+        "local" : "Udemy",
+        "status" : "Cursando",
+        "data" : "Online - jan/2023",
+        "linguagens" : [
+            "TypeScript", "JavaScript",
+            "Angular v+14"
+        ]
+    },
+    {
+        "title" : "Gestão Ágil com Scrum",
+        "local" : "Udemy",
+        "status" : "Cursando",
+        "data" : "Online - jan/2023",
+        "linguagens" : [
+                        "Técnicas Ágeis", "Planejamento de projetos",
+                        "Gerenciamento de projetos",
+                        "Técnica de produtividade"
+        ]
+    },
+    {
+        "title" : "Spring Boot com Angular",
+        "local" : "Udemy",
+        "status" : "Concluído",
+        "data" : "Online - dez/2022 - jan/2023",
+        "linguagens" : [
+                        "API RESTful", 
+                        "DB h2", "Padrão MVC",
+                        "Padrão DTO", "MySQL",
+                        "Tokens JWT", "Spring Data JPA"
+                       ]
+    },
+    {
+        "title" : "Programa Start Capgemini",
+        "local" : "Plataform Programa Start Capgemini",
+        "status" : "Concluído",
+        "data" : "Online - set/2022 - jan/2023",
+        "linguagens" : [
+                        "Java", "Java Web", 
+                        "Angular", "API", 
+                        "HTML Avançado", "Css Avançado", 
+                        "JavaScript"
+                       ]
+    },
+    {
+        "title" : "Java Web com JSP e MySQL",
+        "local" : "Udemy",
+        "status" : "Concluído",
+        "data" : "Online - nov/2022 - dez/2023",
+        "linguagens" : [
+                        "JSP", "HTML", "CSS", 
+                        "MySQL", "Padrão MVC",
+                        "Requisições HTTP", "Persistencia de dados",
+                        "Ajax", "Xamp PhpAdmim"
+                       ]
+    },
+    
+    {
+        "title" : "Linguagem Python",
+        "local" : "Hastag Treinamentos",
+        "status" : "Concluído",
+        "data" : "Online - mai/2022 - ago/2022",
+        "linguagens" : [
+            "Pandas", "Flask",
+            "POO", "APIs",
+            "APIs", "Integração com Excel",
+            "Integração com PDF", "Arqivos CSV ",
+            "Arquivos JSON", "Integração com ArcGIS"
+        ]
+    },
+    {
+        "title" : "Basico JavaScript",
+        "local" : "Digital Innovation One",
+        "status" : "Concluído",
+        "data" : "Online - jul/2022 - ago /2022",
+        "linguagens" : [
+            "JavaScript básico","HTML básico ","CSS básico ",
+        ]
+    },
+    
+    {
+        "title" : "English LIve",
+        "local" : "Becas Santander",
+        "status" : "Concluído",
+        "data" : "Online - jul/2022",
+        "linguagens" : []
+    }
+
+]
---
+++ src/app/data/modelData.ts
@@ -0,0 +1,61 @@
+export const modelDialogData = [
+
+    {
+        "id": "1",
+        "title": "Projeto Acadêmico",
+        "dataInicio": "jan/2023",
+        "dataFim": "jan/2023",
+        "status": "Em andamento",
+        "cargo": "Desenvolvedor",
+        "atividades": [
+            "Definição do projeto básico",
+            "Requisitos do projeto",
+            "Regras de negócio",
+            "Criação do Banco de Dados",
+            "Criação das Classes de models",
+            "Criação da conexão com banco de dados",
+            "Criação das páginas",
+            "Criação dos icones Habilidades",
+            "Validações",
+            "Ajustes finais",
+            "Metodologia Scrum"
+        ],
+        "descricao": 'Este projeto tem o objetivo de fortalecer o apredizado das linguagems <b style="color: blue">Java, Angular</b>, <b>Css, HTML</b> e <b>JavaScrip</b>, com a criação de uma aplicação Web. Será usada a <b>IDE IntelliJ. </b>'
+    }
+  ,
+    {
+        "id": "2",
+        "title": "Portfólio em Java JSP",
+        "dataInicio": "out/2022",
+        "dataFim": "nov/2022",
+        "status": "Concluído",
+        "cargo": "Desenvolvedor",
+        "atividades": [
+            "Criação e Administração do portfólio",
+            "Criação das Classes de models",
+            "Criação do DataBase",
+            "Conexão com o DataBase",
+            "Criação daas operações de persistencia de dados",
+            "Design das Pages",
+        ],
+        "descricao": 'Esta implementação foi criada com a linguagem <b class="b-color">Java</b>(<b> JSP e JDBC)</b> uso do <b>Bootstrap, HTML, CSS, JavaScript</b> e versionamento no <b>GitHub</b>. Contruida com a <b>IDE Netbeans</b>.'
+    },
+    {
+        "id": "3",
+        "title": "Portal Master Tecnology",
+        "dataInicio": "jul/2022",
+        "dataFim": "ago/2022",
+        "status": "Concluído",
+        "cargo": "Desenvolvedor",
+        "atividades": [
+            "Criação e Administração",
+            "Criação do Banco de Dados",
+            "Criação das Classes e Criação Crud",
+            "Login Usuário com Flask_login",
+            "Design das Pages",
+            "Consumo de API"
+        ],
+        "descricao": 'Esta implementação foi criada com a linguagem <b class="b-color">Python</b>, Login de usuário com <b>Flask_login, Git, GitHub, HTML, CSS, JavaScript, Heroku</b>. Contruida com a <b>IDE IntelliJ</b>.'
+    }
+
+]

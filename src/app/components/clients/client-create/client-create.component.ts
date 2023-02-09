8f9353b50773e5d6fe96b37d331a377f48b61af7
ClaudioCarige <ccarige@gmail.com>
2023-02-09 01:27:35 -0300

    Model Client criado

--- src/app/components/clients/client-create/client-create.component.ts
+++ src/app/components/clients/client-create/client-create.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit        } from '@angular/core';
+import { FormControl, Validators  } from '@angular/forms';
+import { Router                   } from '@angular/router';
+import { ToastrService            } from 'ngx-toastr';
+import { Client                   } from 'src/app/models/dataClient';
+import { ClientsService           } from 'src/app/services/clients.service';
 
 @Component({
   selector: 'app-client-create',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientCreateComponent implements OnInit {
 
-  constructor() { }
+  client: Client = {
+    id:           '',
+    name:         '',
+    cpf:          '',
+    email:        '',
+    password:     '',
+    profile:      [],
+    criationDate: ''
+  }
+  name:     FormControl = new FormControl(null, Validators.minLength(3));
+  cpf:      FormControl = new FormControl(null, [Validators.required, Validators.minLength(11)]);
+  email:    FormControl = new FormControl(null,        Validators.email);
+  password: FormControl = new FormControl(null, Validators.minLength(6));
+
+  constructor(
+    private service: ClientsService,
+    private toast: ToastrService,
+    private route: Router
+  ) { }
 
   ngOnInit(): void {
   }
+  create(): void {
+    this.service.create(this.client).subscribe(() => {
+      this.toast.success('Cliente criado com sucesso!', 'Cadastro')
+      this.route.navigate(['clients']);
+    }, ex => {
+      console.log(ex);
+      if (ex.error.erros) {
+        ex.error.erros.array.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
 
+  addPerfil(profile: any): void {
+    if (this.client.profile.includes(profile)) {
+      this.client.profile.splice(this.client.profile.indexOf(profile), 1);
+    } else {
+      this.client.profile.push(profile);
+    }
+  }
+  validation(): boolean {
+    return this.name.valid && this.cpf.valid && this.email.valid && this.password.valid;
+  }
 }
---
+++ src/app/models/client.ts
@@ -0,0 +1,9 @@
+export interface Client{
+    id?:             any;
+    name:         string;
+    cpf:          string;
+    email:        string;
+    password:     string;
+    profile:    string[];
+    criationDate:    any;
+}

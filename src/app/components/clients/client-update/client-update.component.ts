b714dfa22356167306f72e1d6c91f2c269c65c26
ClaudioCarige <ccarige@gmail.com>
2023-02-11 09:54:13 -0300

    HTML, CSS e serviços CLIENT-UPDATE criado

--- src/app/components/clients/client-update/client-update.component.ts
+++ src/app/components/clients/client-update/client-update.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Client } from 'src/app/models/modelClient';
+import { ClientsService } from 'src/app/services/clients.service';
+
 
 @Component({
   selector: 'app-client-update',
@@ -6,10 +12,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-update.component.css']
 })
 export class ClientUpdateComponent implements OnInit {
+  client: Client = {
+    id:           '',
+    name:         '',
+    cpf:          '',
+    email:        '',
+    password:     '',
+    profile:      [],
+    criationDate: ''
+  }
+  name:     FormControl = new FormControl(null, Validators.minLength(3));
+  cpf:      FormControl = new FormControl(null,     Validators.required);
+  email:    FormControl = new FormControl(null,        Validators.email);
+  password: FormControl = new FormControl(null, Validators.minLength(4));
 
-  constructor() { }
-
+  constructor(
+    private service:      ClientsService,
+    private toast:        ToastrService,
+    private route:        Router,
+    private activeRoute:  ActivatedRoute
+  ) { }
   ngOnInit(): void {
+    this.client.id = this.activeRoute.snapshot.paramMap.get('id');
+    this.findById();
   }
 
+  findById() {
+    this.service.findById(this.client.id).subscribe(resposta => {
+      resposta.profile = [];
+      this.client = resposta;
+    })
+  }
+  update(): void {
+    this.service.update(this.client).subscribe(() => {
+      this.toast.success('Funcionário atualizado com sucesso!', 'A T U A L I Z A R - F U N C I O N Á R I O');
+      this.route.navigate(['clients'])
+    }, ex => {
+      console.log(ex.error.errors);
+      if (ex.error.errors) {
+        ex.error.errors.array.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  addPerfil(profile: any): void {
+    if (this.client.profile.includes(profile)) {
+      this.client.profile.splice(this.client.profile.indexOf(profile), 1);
+    } else {
+      this.client.profile.push(profile);
+    }
+  }
+  validation(): boolean {
+    return this.name.valid && this.cpf.valid && this.email.valid && this.password.valid;
+  }
 }
+

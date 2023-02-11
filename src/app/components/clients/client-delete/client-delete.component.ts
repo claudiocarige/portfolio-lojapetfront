7d29ebdf38448e265a2e9ef985cacb5c4b3a65c5
ClaudioCarige <ccarige@gmail.com>
2023-02-11 10:02:05 -0300

    HTML, CSS e serviços de CLIENT-DELETE criados

--- src/app/components/clients/client-delete/client-delete.component.ts
+++ src/app/components/clients/client-delete/client-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Client } from 'src/app/models/modelClient';
+import { ClientsService } from 'src/app/services/clients.service';
+
 
 @Component({
   selector: 'app-client-delete',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-delete.component.css']
 })
 export class ClientDeleteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  client: Client = {
+    id:           '',
+    name:         '',
+    cpf:          '',
+    email:        '',
+    password:     '',
+    profile:      [],
+    criationDate: ''
   }
 
+  check: string
+  constructor(
+    private service:      ClientsService,
+    private toast:        ToastrService,
+    private route:        Router,
+    private activeRoute:  ActivatedRoute
+  ) { }
+  ngOnInit(): void {
+    this.client.id = this.activeRoute.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById() {
+    this.service.findById(this.client.id).subscribe(resposta => {
+      resposta.profile = [];
+      this.client = resposta;
+    })
+  }
+  delete(): void {
+    this.service.delete(this.client.id).subscribe(() => {
+      this.toast.success('Funcionário deletado com sucesso!', 'D E L E T A R - F U N C I O N Á R I O');
+      this.route.navigate(['clients'])
+    }, ex => {
+      console.log(ex.error.errors);
+      if (ex.error.errors) {
+        ex.error.errors.array.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  addCheck(): boolean{
+   return this.client.name === this.check
+  }
+  validation(): boolean{
+    return this.client.name === this.check
+  }
 }

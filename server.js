466c48f6613954986e1c3da6b31588fe77e959a2
ClaudioCarige <ccarige@gmail.com>
2023-02-11 15:19:31 -0300

    Adequação para Deploy no Heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const appName = "lojaservicepet";
+const outputPath = `${__dirname}/dist/${appName}`;
+
+app.use(express.static(outputPath));
+app.get('/*', (req, res) => {
+    res.sendFile(`${outputPath}/index.html`);
+});
+app.listen(process.env.PORT);
